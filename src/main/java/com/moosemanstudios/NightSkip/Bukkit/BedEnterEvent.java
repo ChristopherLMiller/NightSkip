2181ae4920dc01aa9aa3769dd834c9482063d956
BuildTools <unconfigured@null.spigotmc.org>
2016-04-30 13:52:54 -0500

    fixes for getOnlinePlayer

--- src/main/java/com/moosemanstudios/NightSkip/Bukkit/BedEnterEvent.java
+++ src/main/java/com/moosemanstudios/NightSkip/Bukkit/BedEnterEvent.java
@@ -3,6 +3,7 @@ package com.moosemanstudios.NightSkip.Bukkit;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
@@ -17,7 +18,7 @@ public class BedEnterEvent implements Listener {
 	@EventHandler
 	public void onBedEnter(PlayerBedEnterEvent event) {
 		// see if there is more than just the one person online, if there isn't we can just let sleeping occur naturally.
-		if ((Bukkit.getServer().getOnlinePlayers().length > 1) && (event.getPlayer().hasPermission("nightskip.skip")) && plugin.skipBedEnter) {
+		if ((getPlayersOnline() > 1) && (event.getPlayer().hasPermission("nightskip.skip")) && plugin.skipBedEnter) {
 			Bukkit.getServer().dispatchCommand((CommandSender) event.getPlayer(), "skip");
 			event.getPlayer().sendMessage(ChatColor.YELLOW + "You've requested the jump to day");
 			event.setCancelled(true);
@@ -28,4 +29,21 @@ public class BedEnterEvent implements Listener {
 			}
 		}
 	}
+
+	private int getPlayersOnline() {
+		int playersOnline = 0;
+		try {
+			if (Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).getReturnType() == Collection.class)
+				playersOnline = ((Collection<?>)Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).invoke(null, new Object[0])).size();
+			else
+				playersOnline = ((Player[])Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).invoke(null, new Object[0])).length;
+		}
+		catch (NoSuchMethodException ex){} // can never happen
+		catch (InvocationTargetException ex){} // can also never happen
+		catch (IllegalAccessException ex){} // can still never happen
+
+        return playersOnline;
+	}
+
+
 }
