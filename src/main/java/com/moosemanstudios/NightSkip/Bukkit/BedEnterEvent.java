f7a4e91c6458a45b1600ee64251d88cdf4d00606
BuildTools <unconfigured@null.spigotmc.org>
2016-05-02 14:52:24 -0500

    fixes for getOnlinePlayer

--- src/main/java/com/moosemanstudios/NightSkip/Bukkit/BedEnterEvent.java
+++ src/main/java/com/moosemanstudios/NightSkip/Bukkit/BedEnterEvent.java
@@ -18,7 +18,7 @@ public class BedEnterEvent implements Listener {
 	@EventHandler
 	public void onBedEnter(PlayerBedEnterEvent event) {
 		// see if there is more than just the one person online, if there isn't we can just let sleeping occur naturally.
-		if ((getPlayersOnline() > 1) && (event.getPlayer().hasPermission("nightskip.skip")) && plugin.skipBedEnter) {
+		if ((Bukkit.getServer()._INVALID_getOnlinePlayers().length > 1) && (event.getPlayer().hasPermission("nightskip.skip")) && plugin.skipBedEnter) {
 			Bukkit.getServer().dispatchCommand((CommandSender) event.getPlayer(), "skip");
 			event.getPlayer().sendMessage(ChatColor.YELLOW + "You've requested the jump to day");
 			event.setCancelled(true);
@@ -29,21 +29,5 @@ public class BedEnterEvent implements Listener {
 			}
 		}
 	}
-
-	private int getPlayersOnline() {
-		int playersOnline = 0;
-		try {
-			if (Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).getReturnType() == Collection.class)
-				playersOnline = ((Collection<?>)Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).invoke(null, new Object[0])).size();
-			else
-				playersOnline = ((Player[])Bukkit.class.getMethod("getOnlinePlayers", new Class<?>[0]).invoke(null, new Object[0])).length;
-		}
-		catch (NoSuchMethodException ex){} // can never happen
-		catch (InvocationTargetException ex){} // can also never happen
-		catch (IllegalAccessException ex){} // can still never happen
-
-        return playersOnline;
-	}
-
 
 }
